b152694db8d4e95c6ebb8b247716823f6376e044
Louise345 <87196093+Louise345@users.noreply.github.com>
2021-07-09 11:41:08 -0400

    Initial files for MakeCode project

---
+++ README.md
@@ -0,0 +1,33 @@
+ 
+
+
+> Open this page at [https://louise345.github.io/space-invaders-update/](https://louise345.github.io/space-invaders-update/)
+
+## Use as Extension
+
+This repository can be added as an **extension** in MakeCode.
+
+* open [https://arcade.makecode.com/](https://arcade.makecode.com/)
+* click on **New Project**
+* click on **Extensions** under the gearwheel menu
+* search for **https://github.com/louise345/space-invaders-update** and import
+
+## Edit this project ![Build status badge](https://github.com/louise345/space-invaders-update/workflows/MakeCode/badge.svg)
+
+To edit this repository in MakeCode.
+
+* open [https://arcade.makecode.com/](https://arcade.makecode.com/)
+* click on **Import** then click on **Import URL**
+* paste **https://github.com/louise345/space-invaders-update** and click import
+
+## Blocks preview
+
+This image shows the blocks code from the last commit in master.
+This image may take a few minutes to refresh.
+
+![A rendered view of the blocks](https://github.com/louise345/space-invaders-update/raw/master/.github/makecode/blocks.png)
+
+#### Metadata (used for search, rendering)
+
+* for PXT/arcade
+<script src="https://makecode.com/gh-pages-embed.js"></script><script>makeCodeRender("{{ site.makecode.home_url }}", "{{ site.github.owner_name }}/{{ site.github.repository_name }}");</script>
---
+++ main.ts
@@ -0,0 +1,132 @@
+controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
+    blockMenu.showMenu(["store", ""], MenuStyle.Grid, MenuLocation.FullScreen)
+})
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    projectile = sprites.createProjectileFromSprite(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . 5 5 5 . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `, Spaceships, 0, -44)
+})
+sprites.onOverlap(SpriteKind.Enemy, SpriteKind.Projectile, function (sprite, otherSprite) {
+    sprite.destroy()
+    info.changeScoreBy(1)
+})
+sprites.onOverlap(SpriteKind.Enemy, SpriteKind.Player, function (sprite, otherSprite) {
+    sprite.destroy()
+    info.changeLifeBy(-1)
+})
+blockMenu.onMenuOptionSelected(function (option, index) {
+    if (blockMenu.selectedMenuOption() == "store") {
+        blockMenu.showMenu(["1 life for 3 points", "2 lives for 5 points", "3 lives for 7 points"], MenuStyle.Grid, MenuLocation.FullScreen)
+    } else {
+        if (blockMenu.isMenuOpen()) {
+            if (index == 0 && info.score() >= 3) {
+                info.changeLifeBy(index + 1)
+                info.changeScoreBy(-3)
+            } else if (index == 1 && info.score() >= 5) {
+                info.changeLifeBy(index + 1)
+                info.changeScoreBy(-5)
+            } else if (index == 2 && info.score() >= 7) {
+                info.changeLifeBy(index + 1)
+                info.changeScoreBy(-7)
+            }
+        }
+        blockMenu.closeMenu()
+    }
+})
+let invaders: Sprite = null
+let projectile: Sprite = null
+let Spaceships: Sprite = null
+Spaceships = sprites.create(img`
+    . . . . . . . c d . . . . . . . 
+    . . . . . . . c d . . . . . . . 
+    . . . . . . . c d . . . . . . . 
+    . . . . . . . c b . . . . . . . 
+    . . . . . . . f f . . . . . . . 
+    . . . . . . . c 2 . . . . . . . 
+    . . . . . . . f f . . . . . . . 
+    . . . . . . . e 2 . . . . . . . 
+    . . . . . . e e 4 e . . . . . . 
+    . . . . . . e 2 4 e . . . . . . 
+    . . . . . c c c e e e . . . . . 
+    . . . . e e 2 2 2 4 e e . . . . 
+    . . c f f f c c e e f f e e . . 
+    . c c c c e e 2 2 2 2 4 2 e e . 
+    c c c c c c e e 2 2 2 4 2 2 e e 
+    c c c c c c e e 2 2 2 2 4 2 e e 
+    `, SpriteKind.Player)
+controller.moveSprite(Spaceships)
+Spaceships.setStayInScreen(true)
+Spaceships.y = 120
+let list = [img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . 5 . . 5 . . . . . . . . 
+    . . . . 5 . . 5 . . . . . . . . 
+    . . . . 7 7 7 7 . . . . . . . . 
+    . . . . f 7 7 f . . . . . . . . 
+    . . . . 7 7 7 7 . . . . . . . . 
+    . . . . 7 7 7 7 . . . . . . . . 
+    . . . . 7 7 7 7 . . . . . . . . 
+    . . . . 7 7 7 7 . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `, img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . 5 . . 5 . . . . . . . . 
+    . . . . 5 . . 5 . . . . . . . . 
+    . . . . 7 7 7 7 . . . . . . . . 
+    . . . . f 7 7 f . . . . . . . . 
+    . . . . 7 7 7 7 . . . . . . . . 
+    . . . . 7 7 7 7 . . . . . . . . 
+    . . . . 7 7 7 7 . . . . . . . . 
+    . . . . 7 7 7 7 . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `, img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . 5 . . 5 . . . . . . . . 
+    . . . . 5 . . 5 . . . . . . . . 
+    . . . . 7 7 7 7 . . . . . . . . 
+    . . . . f 7 7 f . . . . . . . . 
+    . . . . 7 7 7 7 . . . . . . . . 
+    . . . . 7 7 7 7 . . . . . . . . 
+    . . . . 7 7 7 7 . . . . . . . . 
+    . . . . 7 7 7 7 . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `]
+info.setLife(5)
+game.onUpdateInterval(2000, function () {
+    invaders = sprites.createProjectileFromSide(list[randint(0, 2)], 0, 50)
+    invaders.setPosition(randint(20, 140), 0)
+    invaders.setKind(SpriteKind.Enemy)
+})
